347d9721dd26cac7811f6ed24399a5fb303d3a27
John Wen <John.Wen@ehealth.com>
2018-11-15 09:29:50 +0800

    .

--- app/user/models.py
+++ app/user/models.py
@@ -1,11 +1,9 @@
 from app import db
 from werkzeug.security import generate_password_hash,check_password_hash
-# from flask_login import UserMixin
 from flask import url_for
-
-# @login.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
+from datetime import datetime,timedelta
+import base64
+import os 
 
 class PagenationAPIMixin(object):
      @staticmethod
@@ -37,6 +35,29 @@ class User(PagenationAPIMixin,db.Model):
     password_hash = db.Column(db.String(256))
     email = db.Column(db.String(50),unique=True,index=True)
     posts = db.relationship('Post',backref='author',lazy='dynamic')
+    token = db.Column(db.String(32))
+    token_expire = db.Column(db.DateTime)
+
+    def get_token(self,expires_in=3600):
+        now = datetime.now()
+        if self.token and self.token_expire > now + timedelta(seconds=60):
+            return self.token 
+        
+        self.token= base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expire = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+    
+    def revoke_token(self):
+        self.token_expire = datetime.now() - timedelta(seconds=1)
+    
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if not user or user.token_expire < datetime.now():
+            return None
+        
+        return user
 
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
@@ -44,8 +65,7 @@ class User(PagenationAPIMixin,db.Model):
     def get_password(self,password):
         return check_password_hash(self.password_hash,password)
 
-    def __str__(self):
-        return "<username: %s>".format(self.username)
+
 
     def to_dict(self):
         data =  {
--- app/user/routes.py
+++ app/user/routes.py
@@ -1,4 +1,4 @@
 from . import user_bp
 from .views import Users,GetUser
-user_bp.add_url_rule('/get-users',view_func=Users.as_view('get_users'))
+user_bp.add_url_rule('/get_users',view_func=Users.as_view('get_users'))
 user_bp.add_url_rule('/user/<int:user_id>',view_func=GetUser.as_view('get_user'))
